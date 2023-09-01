2c11ed1c07d8bcd2eb769c19913652820d38004e
joneill3877 <joneill3877@charlottesvilleschools.org>
2023-09-01 15:12:33 -0400

    Captouch working with servo

--- hello.py
+++ hello.py
@@ -10,20 +10,19 @@ import pwmio
 from adafruit_motor import servo
 
 # create a PWMOut object on Pin A2.
-pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
+pwm = pwmio.PWMOut(board.D9, duty_cycle=2 ** 15, frequency=50)
 
 # Create a servo object, my_servo.
-my_servo = servo.Servo(pwm)
+my_servo = servo.ContinuousServo(pwm)
 
-touch_A4 = touchio.TouchIn(board.A4)  # Not a touch pin on Trinket M0!
+touch_A1 = touchio.TouchIn(board.A1)  # Not a touch pin on Trinket M0!
 touch_A5 = touchio.TouchIn(board.A5)  # Not a touch pin on Trinket M0!
 
 while True:
-    if touch_A4.value:
-        for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
-            my_servo.angle = angle
-            time.sleep(0.05)
-    if touch_A5.value:
-        for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
-            my_servo.angle = angle
-            time.sleep(0.05)
+    my_servo.throttle = 0.0
+    while touch_A1.value:
+        my_servo.throttle = 1.0
+        time.sleep(.5)
+    while touch_A5.value:
+        my_servo.throttle = -1.0
+        time.sleep(.5)
